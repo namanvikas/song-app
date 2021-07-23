032e746bbdee9ef189b5df1b5466c74b571d952c
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-07-23 13:22:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftwristX=0;
+leftwristY=0;
+rightwristX=0;
+rightwristY=0;
 function preload(){
     song=loadSound("music2.mp3");
 }
@@ -8,13 +12,40 @@ function setup(){
     Canvas.center();
 
     video=createCapture(VIDEO);
-    video.hise();
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    pose.on('pose',gotposes);
 }
 
-function draw()[
+function draw(){
     image(video,0,0,500,600);
-]
+}
+
+function modelloaded(){
+    console.log('model is Initialised')
+}
 
 function play(){
     song.play();
-}
+    song.setvolume(1);
+    song.rate(1);
+    }
+
+    function gotposes(reults){
+        if(results.length>0){
+            console.log(results);
+            leftwristX=results[0].pose.leftWrist.x;
+            leftwristY=results[0].pose.leftWrist.y;
+            console.log("leftwristX="+leftwristX+",leftwristY="+leftwristY);
+
+            rightwristX=results[0].pose.rightWrist.x;
+            rightWristY=results[0].pose.rightWrist.y;
+            console.log("rightwristY="+rightwristX+"rightwristY="+rightwristY);
+
+
+        }
+            
+    }
+    
+    
