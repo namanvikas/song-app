bb0b39170f22a36849398fbed487eed2d9615699
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-07-30 20:09:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftwristY=0;
 rightwristX=0;
 rightwristY=0;
 scoreleftwrist=0;
+scorerightwrist=0
 function preload(){
     song=loadSound("music2.mp3");
 }
@@ -23,6 +24,29 @@ function draw(){
     image(video,0,0,500,600);
     fill("#ff0000");
     stroke("#ff0000");
+    circle(rightwristX,rightwristY,20);
+    if(rightwristY>0 && rightwristY<=100){
+        document.getElementById("speed").innerHTML="speed:0.5x";
+        song.rate(0.5);
+    }
+    else if(rightwristY>100 && rightwristY<=200){
+        document.getElementById("speed").innerHTML="speed:1x";
+        song.rate(1);
+    }
+
+    else if(rightwristY>200 && rightwristY<=300){
+        document.getElementById("speed").innerHTML="speed:1.5x";
+        song.rate(1.5);
+    }
+    else if(rightwristY>300 && rightwristY<=400){
+        document.getElementById("speed").innerHTML="speed:2x";
+        song.rate(2);
+    }
+    else if(rightwristY>400 && rightwristY<=500){
+        document.getElementById("speed").innerHTML="speed:2.5x";
+        song.rate(2.5);
+    }
+}
     if(scoreleftwrist>0.2){
 
 
@@ -50,7 +74,8 @@ function play(){
         if(results.length>0){
             console.log(results);
 scoreleftwrist=results[0].pose.keypoints[9].score;
-console.log("scoreleftwrist="+scoreleftwrist);
+scorerightwrist=results[0].pose.keypoints[10].score;
+console.log("scoreleftwrist="+scoreleftwrist+"scorerightwrist="+scorerightwrist);
             leftwristX=results[0].pose.leftWrist.x;
             leftwristY=results[0].pose.leftWrist.y;
             console.log("leftwristX="+leftwristX+",leftwristY="+leftwristY);
